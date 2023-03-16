da269c2333c9d7141b316d958e1ad54a34a54376
Bereket Yimer <bekym0242@gmail.com>
2023-03-16 17:28:29 +0300

    inventory table look touch

--- src/views/admin/inventory/index.tsx
+++ src/views/admin/inventory/index.tsx
@@ -1,17 +1,11 @@
-import { Box, Grid } from "@chakra-ui/react";
-import { Col, Row } from "antd";
+import { Box } from "@chakra-ui/react";
 import InventoryTable from "./components/InventoryTable";
-import { chiefKitchenItem, baristaKitchenItem } from "./variable/const";
 
 export default function Overview() {
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       {/* Main Fields */}
-      <Row>
-        <Col span={12}>
-          <InventoryTable />
-        </Col>
-      </Row>
+      <InventoryTable />
     </Box>
   );
 }
